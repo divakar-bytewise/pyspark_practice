076cd0115cb37fec51272fb45be7f8884b8417a8
diakar-bytewise <divakar.c@diggibyte.com>
2025-07-11 09:31:59 +0530

    the round function is used to round up the decimal value as we need and round used with select function.

---
+++ src/round_operation.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+import os
+os.environ['PYSPARK_PYTHON'] = r'C:\Users\DivakarC\AppData\Local\Programs\Python\Python311\python.exe'
+os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Users\DivakarC\AppData\Local\Programs\Python\Python311\python.exe'
+
+spark=SparkSession.builder.appName("Round Operations").getOrCreate()
+
+data=[('Divakar',23,23090.123,'DE'),('Edwin',22,24500.432,'DS'),('Stewart',22,25000.232,'DE')]
+schema=['Name','Age','Salary','Role']
+df=spark.createDataFrame(data,schema)
+df1=df.select('Name',round('Salary',1).alias('Rounded_salary'))
+df1.show()
