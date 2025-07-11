f4f12117189867d1314e27e6b19e789b123c9895
diakar-bytewise <divakar.c@diggibyte.com>
2025-07-11 09:39:59 +0530

    the Itrim function is used to removes the left whitespace from a string.

---
+++ src/Itrim_operation.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import os
+os.environ['PYSPARK_PYTHON'] = r'C:\Users\DivakarC\AppData\Local\Programs\Python\Python311\python.exe'
+os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Users\DivakarC\AppData\Local\Programs\Python\Python311\python.exe'
+
+spark=SparkSession.builder.appName("Ltrim operations").getOrCreate()
+data=[(' Divakar ',23,23090,'DE'),('    Edwin ',22,24500,'DS'),('   Stewart ',22,25000,'DE')]
+schema=['Name','Age','Salary','Role']
+df=spark.createDataFrame(data,schema)
+df1=df.select('Name',ltrim('Name').alias('Trimmed_Name'))
+df1.show()
