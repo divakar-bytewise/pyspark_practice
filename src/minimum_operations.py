656ccccb9bc78c70ba7ce8dc4553ffaf8ed245e0
diakar-bytewise <divakar.c@diggibyte.com>
2025-07-11 09:25:25 +0530

    the min function is used to find the column min value and min used with agg function.

---
+++ src/minimum_operations.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+import os
+os.environ['PYSPARK_PYTHON'] = r'C:\Users\DivakarC\AppData\Local\Programs\Python\Python311\python.exe'
+os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Users\DivakarC\AppData\Local\Programs\Python\Python311\python.exe'
+
+spark=SparkSession.builder.appName("Minimum Operations").getOrCreate()
+
+data=[('Divakar',23,23090,'DE'),('Edwin',22,24500,'DS'),('Stewart',22,25000,'DE')]
+schema=['Name','Age','Salary','Role']
+df=spark.createDataFrame(data,schema)
+df1=df.select(min("Salary").alias("Min_Expense"))
+df2=df.groupby('Role').agg(min('Salary').alias('Min_salary'))
+df1.show()
+df2.show()
